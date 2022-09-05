6df8c573d81ddc45c828d19814d13533d8f719d2
Ghullam abbas <ghulam.abbas@sigmatec.com.pk>
2022-09-05 15:59:50 +0500

    enable app for mac os

---
+++ Africa/Modifier/CenterModifier.swift
@@ -0,0 +1,20 @@
+//
+//  CenterModifier.swift
+//  Africa
+//
+//  Created by Developer on 05/09/2022.
+//
+
+import SwiftUI
+
+struct CenterModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CenterModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        CenterModifier()
+    }
+}
---
+++ Africa/Views/CreditView.swift
@@ -0,0 +1,20 @@
+//
+//  CreditView.swift
+//  Africa
+//
+//  Created by Developer on 05/09/2022.
+//
+
+import SwiftUI
+
+struct CreditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreditView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditView()
+    }
+}
